c380a306f4f206dcd6992d2b0c05876c6d36a8fd
friday <friday@192.168.1.4>
2025-08-08 12:59:48 +0700

    feat: saved username with shared preferences

--- app/src/main/java/com/luckyfriday/vocabulary/MainActivity.kt
+++ app/src/main/java/com/luckyfriday/vocabulary/MainActivity.kt
@@ -1,20 +1,64 @@
 package com.luckyfriday.vocabulary
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.edit
+import androidx.core.view.isVisible
+import com.luckyfriday.vocabulary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bindingMain: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        bindingMain = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingMain.root)
+
+        val sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE)
+        val savedUsername = sharedPreferences.getString("USERNAME", null)
+        if (savedUsername != null) {
+            // direct to main app
+        } else {
+            showOnboard()
+        }
+    }
+
+    private fun showOnboard() {
+        bindingMain.etNameOnboarding.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                bindingMain.tvTitle.text = getString(R.string.tell_me_your_name)
+                bindingMain.btnStart.isVisible = !s.isNullOrEmpty()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                bindingMain.tvTitle.text = getString(R.string.introduction_your_name)
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                bindingMain.tvTitle.text = getString(R.string.tell_me_your_name)
+            }
+        })
+        bindingMain.btnStart.setOnClickListener {
+            val username = bindingMain.etNameOnboarding.text.toString()
+            saveName(username)
+        }
+    }
+
+    private fun saveName(username: String) {
+        val sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE)
+//        val editor = sharedPreferences.edit()
+//        editor.putString("USERNAME", username)
+//        editor.apply()
+        sharedPreferences.edit {
+            putString("USERNAME", username)
         }
     }
 }
